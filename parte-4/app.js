a1542f717cb3e3599c990a4dfd9afdbfbf129669
Igor Rocha <igortice@gmail.com>
2018-02-17 19:43:27 -0300

    better 2 parte 4

--- parte-4/app.js
+++ parte-4/app.js
@@ -31,7 +31,7 @@ greet();
 var greetFriend = function (friend) { return console.log(friend); };
 greetFriend('manu');
 console.log('DEFAULT PARAMS');
-// DEFAULT PARAMS
+// default params
 var countDown = function (start) {
     if (start === void 0) { start = 10; }
     console.log(start);
@@ -42,4 +42,38 @@ var countDown = function (start) {
 };
 countDown();
 countDown(20);
+console.log('REST & SPREAD');
+// rest & spread
+var numbers = [1, 22, -5, 9];
+console.log(Math.max(1, 22, -5, 9));
+console.log(Math.max.apply(Math, numbers)); // spread
+// function makeArray(arg1: number, arg2: number) {
+//   return [arg1, arg2];
+// }
+// console.log(makeArray(1, 2));
+function makeArray() {
+    var args = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        args[_i] = arguments[_i];
+    }
+    return args;
+}
+console.log(makeArray(1, 2));
+// Destructuring
+console.log('DESTRUCTURING');
+var myHobbies = ['Programacao', 'Gaming'];
+// const hob1= myHobbies[0];
+// const hob2= myHobbies[1];
+var hob1 = myHobbies[0], hob2 = myHobbies[1];
+console.log(hob1, hob2);
+var userData = { userName: 'igor', userAge: 37 };
+// const userName = userData.userName;
+// const userAge  = userData.age;
+var userName = userData.userName, userAge = userData.userAge;
+console.log(userName, userAge);
+// template literals
+var myName = 'igor rocha';
+// const hello = 'Ola meu nome é ' + myName;
+var hello = "Verry complex string\nOla meu nome \u00E9 " + myName + "\n \n";
+console.log(hello);
 //# sourceMappingURL=app.js.map
--- parte-4/app.ts
+++ parte-4/app.ts
@@ -39,7 +39,7 @@ const greetFriend = friend => console.log(friend);
 greetFriend('manu');
 
 console.log('DEFAULT PARAMS');
-// DEFAULT PARAMS
+// default params
 const countDown = (start: number = 10): void => {
   console.log(start);
   while (start > 0) {
@@ -49,3 +49,43 @@ const countDown = (start: number = 10): void => {
 };
 countDown();
 countDown(20);
+
+console.log('REST & SPREAD');
+// rest & spread
+const numbers = [1, 22, -5, 9];
+console.log(Math.max(1, 22, -5, 9));
+console.log(Math.max(...numbers)); // spread
+
+// function makeArray(arg1: number, arg2: number) {
+//   return [arg1, arg2];
+// }
+// console.log(makeArray(1, 2));
+function makeArray(...args: number[]) {
+  return args;
+}
+
+console.log(makeArray(1, 2));
+
+
+// Destructuring
+console.log('DESTRUCTURING');
+const myHobbies = ['Programacao', 'Gaming'];
+// const hob1= myHobbies[0];
+// const hob2= myHobbies[1];
+const [hob1, hob2] = myHobbies;
+console.log(hob1, hob2);
+
+const userData = { userName: 'igor', userAge: 37 };
+// const userName = userData.userName;
+// const userAge  = userData.age;
+const { userName, userAge } = userData;
+console.log(userName, userAge);
+
+// template literals
+const myName = 'igor rocha';
+// const hello = 'Ola meu nome é ' + myName;
+const hello  = `Verry complex string
+Ola meu nome é ${myName}
+ 
+`;
+console.log(hello);
