97ac860d0ba3b712a96633812d4a864a87410ab8
Igor Rocha <igortice@gmail.com>
2018-02-18 15:35:15 -0300

    better import part 6 modules

--- parte-6/modules/app.ts
+++ parte-6/modules/app.ts
@@ -1,5 +1,6 @@
-import { PI, calculateCircumference } from "./math/circle"
+import * as Circle from './math/circle'; // import { PI, calculateCircumference } from './math/circle';
+import calcRect from './math/rectangle';
 
-console.log(PI);
-console.log(calculateCircumference(10));
-// console.log();
+console.log(Circle.PI);
+console.log(Circle.calculateCircumference(10));
+console.log(calcRect(20, 50));
