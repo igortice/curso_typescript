1491922e5145e30dfdbbc3cc75c7705ff6c2ca5b
igor rocha <igortice@gmail.com>
2018-02-22 09:03:33 -0300

    better interfaces

--- interfaces/app.js
+++ interfaces/app.js
@@ -27,6 +27,7 @@ var p1 = {
 };
 // p1.x = 5; // error!
 console.log(p1);
+// example - 4
 // error: 'colour' not expected in type 'SquareConfig'
 // let mySquare = createSquare({ colour: "red", width: 100 });
 var mySquare2 = createSquare({
@@ -34,4 +35,67 @@ var mySquare2 = createSquare({
     opacity: 0.5
 });
 console.log(mySquare2);
+function createMyUser(config) {
+    return config;
+}
+var myUser = createMyUser({
+    name: 'igor',
+    email: 'igortice@gmail.cm',
+    age: 37
+});
+console.log(myUser);
+var mySearch;
+mySearch = function (src, sub) {
+    var result = src.search(sub);
+    return result > -1;
+};
+console.log(mySearch('igor rocha', 'o'), mySearch('igor rocha', 'b'));
+var myArray;
+myArray = ['Bob', 'Fred'];
+var myStr = myArray[0];
+console.log(myStr);
+function createClock(ctor, hour, minute) {
+    return new ctor(hour, minute);
+}
+var DigitalClock = /** @class */ (function () {
+    function DigitalClock(h, m) {
+    }
+    DigitalClock.prototype.tick = function () {
+        console.log('beep beep');
+    };
+    return DigitalClock;
+}());
+var AnalogClock = /** @class */ (function () {
+    function AnalogClock(h, m) {
+    }
+    AnalogClock.prototype.tick = function () {
+        console.log('tick tock');
+    };
+    return AnalogClock;
+}());
+var digital = createClock(DigitalClock, 12, 17);
+digital.tick();
+var analog = createClock(AnalogClock, 7, 32);
+analog.tick();
+var square = {};
+square.color = 'blue';
+square.sideLength = 10;
+square.penWidth = 5.0;
+console.log(square);
+function getCounter() {
+    var counter = function (start) {
+    };
+    counter.interval = 123;
+    counter.reset = function () {
+    };
+    return counter;
+}
+var c = getCounter();
+console.log(c.interval);
+c(10);
+console.log(c.interval);
+c.reset();
+console.log(c.interval);
+c.interval = 5.0;
+console.log(c.interval);
 //# sourceMappingURL=app.js.map
--- interfaces/app.ts
+++ interfaces/app.ts
@@ -50,13 +50,6 @@ let p1: Point = {
 console.log(p1);
 
 // example - 4
-interface CircleInterface {
-  name?: string;
-  raio?: number;
-
-  [propName: string]: any;
-}
-
 // error: 'colour' not expected in type 'SquareConfig'
 // let mySquare = createSquare({ colour: "red", width: 100 });
 const mySquare2 = createSquare({
@@ -64,3 +57,149 @@ const mySquare2 = createSquare({
   opacity: 0.5
 } as SquareConfig);
 console.log(mySquare2);
+
+
+// example - 5
+
+interface UserInterface {
+  name?: string;
+  email?: string;
+
+  [propName: string]: any;
+}
+
+function createMyUser(config: UserInterface): UserInterface {
+  return config;
+}
+
+const myUser = createMyUser({
+  name:  'igor',
+  email: 'igortice@gmail.cm',
+  age:   37
+});
+
+console.log(myUser);
+
+// example - 6
+interface SearchFunc {
+  (source: string, subString: string): boolean;
+}
+
+let mySearch: SearchFunc;
+mySearch = function (src: string, sub: string): boolean {
+  let result = src.search(sub);
+  return result > -1;
+};
+
+console.log(mySearch('igor rocha', 'o'), mySearch('igor rocha', 'b'));
+
+// example - 7
+interface StringArray {
+  [index: number]: string;
+}
+
+let myArray: StringArray;
+myArray = ['Bob', 'Fred'];
+
+let myStr: string = myArray[0];
+console.log(myStr);
+
+// example - 8
+// interface ClockInterface {
+//   currentTime: Date;
+//
+//   setTime(d: Date);
+// }
+//
+// class Clock implements ClockInterface {
+//   currentTime: Date;
+//
+//   constructor(h: number, m: number) {
+//   }
+//
+//   setTime(d: Date) {
+//     this.currentTime = d;
+//   }
+// }
+
+// example - 9
+interface ClockConstructor {
+  new (hour: number, minute: number): ClockInterface;
+}
+
+interface ClockInterface {
+  tick();
+}
+
+function createClock(ctor: ClockConstructor, hour: number, minute: number): ClockInterface {
+  return new ctor(hour, minute);
+}
+
+class DigitalClock implements ClockInterface {
+  constructor(h: number, m: number) {
+  }
+
+  tick() {
+    console.log('beep beep');
+  }
+}
+
+class AnalogClock implements ClockInterface {
+  constructor(h: number, m: number) {
+  }
+
+  tick() {
+    console.log('tick tock');
+  }
+}
+
+let digital = createClock(DigitalClock, 12, 17);
+digital.tick();
+let analog = createClock(AnalogClock, 7, 32);
+analog.tick();
+
+// example - 10
+interface Shape {
+  color: string;
+}
+
+interface PenStroke {
+  penWidth: number;
+}
+
+interface Square extends Shape, PenStroke {
+  sideLength: number;
+}
+
+let square        = <Square>{};
+square.color      = 'blue';
+square.sideLength = 10;
+square.penWidth   = 5.0;
+console.log(square);
+
+// example - 11
+interface Counter {
+  interval: number;
+
+  (start: number): string;
+
+  reset(): void;
+}
+
+function getCounter(): Counter {
+  let counter      = <Counter>function (start: number) {
+  };
+  counter.interval = 123;
+  counter.reset    = function () {
+  };
+  return counter;
+}
+
+let c = getCounter();
+console.log(c.interval);
+c(10);
+console.log(c.interval);
+c.reset();
+console.log(c.interval);
+c.interval = 5.0;
+console.log(c.interval);
